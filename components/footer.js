3638bd7f7720f951ac8a3d6f7084b4fd84b8da60
Muqadsa Riaz <muqadsariaz@192.168.1.7>
2026-06-08 18:15:18 +0500

    style: final design adjustments in footer

--- components/footer.js
+++ components/footer.js
@@ -6,23 +6,21 @@ export default function Footer() {
     <footer className="site_footer">
       <div className="pg_contain">
         <div className="footer_inner">
-
           <Link href="/" className="footer_brand">
             Vaultix
           </Link>
 
           <div className="footer_copy">
             <p>
-              © 2026 Vaultix Digital Custody. All data is end-to-end encrypted and inaccessible to Vaultix or any third party.
+              © 2026 Vaultix Digital Custody. All data is end-to-end encrypted
+              and inaccessible to Vaultix or any third party.
             </p>
           </div>
 
           <div className="footer_links">
             <Link href="/privacy">Privacy</Link>
             <Link href="/terms">Terms</Link>
-            <Link href="/compliance">Compliance</Link>
           </div>
-
         </div>
       </div>
     </footer>
